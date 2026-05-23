7f4f14a385e4abe37977d5d0ff9cb1a90431b3c8
steve-chavez <stevechavezast@gmail.com>
2026-05-22 20:35:55 -0500

    refactor: put cpc parsers last on test report

--- test/basic.sh
+++ test/basic.sh
@@ -1,10 +1,10 @@
 set -euo pipefail
 
-echo -e "\n=== TEST: basic functionality\n"
-./build/basic.o
-
 echo -e "\n=== TEST: attoparsec csv\n"
 ./build/attoparsec_csv.o < test/samples/customers-100.csv
 
 echo -e "\n=== TEST: attoparsec csv\n"
 ./build/attoparsec_csv.o < test/samples/fewer-fields.csv
+
+echo -e "\n=== TEST: basic functionality\n"
+./build/basic.o
