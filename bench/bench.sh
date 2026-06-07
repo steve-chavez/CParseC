0e05422ec0fb44a98c8eeb71c4f337249db1a0a4
steve-chavez <stevechavezast@gmail.com>
2026-06-07 17:01:28 -0500

    chore: show stderr in bench

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,6 +1,6 @@
 set -euo pipefail
 
-make bench
+make bench > /dev/null
 
 # Haskell, C and Rust should give the same report,
 # this will output a failure showing the differences if there were any.
@@ -15,20 +15,20 @@ diff -u \
 # Print the report in markdown
 echo -e "# Parsing 1M CSV rows"
 
-echo -e "\n## Haskell vs cparsec\n"
+echo -e "\n## Haskell vs CParseC\n"
 
 hyperfine \
   './build/csv_demo.o < bench/data/customers-1000000.csv' \
   './build/attoparsec_csv < bench/data/customers-1000000.csv' \
-  --export-markdown build/report-c-hs.md > /dev/null
+  --export-markdown build/report-c-hs.md 1>&2
 
 cat build/report-c-hs.md
 
-echo -e "\n## Rust vs cparsec\n"
+echo -e "\n## Rust vs CParseC\n"
 
 hyperfine \
   './build/csv_demo.o < bench/data/customers-1000000.csv' \
   './build/csv-rust-demo < bench/data/customers-1000000.csv' \
-  --export-markdown build/report-c-rust.md > /dev/null
+  --export-markdown build/report-c-rust.md 1>&2
 
 cat build/report-c-rust.md
