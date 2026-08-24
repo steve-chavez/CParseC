c09224b0152330555ce56e09f6075e23bcfb6af3
steve-chavez <stevechavezast@gmail.com>
2026-08-23 20:56:30 -0500

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/e6f23dc08d3624daab7094b701aa3954923c6bbb";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forAllSystems (system: {
+        default = import ./shell.nix { pkgs = nixpkgs.legacyPackages.${system}; };
+      });
+    };
+}
--- shell.nix
+++ shell.nix
@@ -1,8 +1,14 @@
-with import (builtins.fetchTarball {
-  name = "2025-06-16";
-  url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
-  sha256 = "sha256:0m0xmk8sjb5gv2pq7s8w7qxf7qggqsd3rxzv3xrqkhfimy2x7bnx";
-}) {};
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  nixpkgsLock = lock.nodes.${lock.nodes.root.inputs.nixpkgs}.locked;
+in
+{ pkgs ? import (builtins.fetchTarball {
+    name = nixpkgsLock.rev;
+    url = "https://github.com/${nixpkgsLock.owner}/${nixpkgsLock.repo}/archive/${nixpkgsLock.rev}.tar.gz";
+    sha256 = nixpkgsLock.narHash;
+  }) {}
+}:
+with pkgs;
 mkShellNoCC {
   buildInputs =
     [
