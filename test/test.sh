20a318c1f9a1ef040a0af2e27851778db1547580
steve-chavez <stevechavezast@gmail.com>
2026-08-30 21:25:12 -0500

    test: exclude fil-c runtime symbols
    otherwise it fails the freestanding test

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,8 @@ echo -e "\n=== TEST: linking works correctly\n"
 ./build/linkage
 
 echo -e "\n=== TEST: freestanding symbol hygiene\n"
-if nm -u build/freestanding | grep .; then
+# Fil-C adds calls to its runtime for safety checks even for freestanding, so we exclude the `fil_c/verse_` prefixes.
+if nm -u build/freestanding | awk '$NF !~ /^(filc_|verse_)/ { print; found = 1 } END { exit !found }'; then
   exit 1
 fi
 
